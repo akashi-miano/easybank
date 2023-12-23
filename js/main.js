a9a7450f633e4928aaf5f929ec3ae13cfa2225a1
akashi <akashi-miano@gmail.com>
2023-12-23 22:45:23 +0800

    Great exercise from frontend mentors!

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const footerNav = document.querySelector(".footer__nav");
+const footerSocialWrapper = document.querySelector(".footer__socials-wrapper");
+const footerList = document.querySelector(".footer__list");
+const mobileHeroImage = document.querySelector(".hero .bg-img");
+const mobileNav = document.getElementById("mobile-nav");
+const mobileToggle = document.querySelector(".nav-toggle");
+
+const updateClasses = () => {
+  if (window.innerWidth <= 750) {
+    footerNav.classList.add("flow-content--m");
+    footerSocialWrapper.classList.add("flow-content--m");
+    footerList.classList.add("flow-content--m");
+    mobileHeroImage.style = "display: none";
+  }
+};
+
+mobileToggle.addEventListener("click", () => {
+  mobileNav.classList.toggle("clicked");
+  if (mobileNav.classList.contains("clicked")) {
+    mobileToggle.querySelector("img").src = "images/icon-close.svg";
+    mobileNav.classList.add("flex");
+  } else {
+    mobileToggle.querySelector("img").src = "images/icon-hamburger.svg";
+    mobileNav.classList.remove("flex");
+  }
+});
+updateClasses();
